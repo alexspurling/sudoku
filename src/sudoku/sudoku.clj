365b08bcbef99dbdfa26242ef7501c1f2892f916
Alex Spurling <alexspurling@gmail.com>
2016-02-04 20:24:15 +0000

    Implemented depth first search. Actually solves the puzzle but takes about 70 minutes.

--- src/sudoku/sudoku.clj
+++ src/sudoku/sudoku.clj
@@ -14,17 +14,17 @@
    [0 0 5  0 1 0  3 0 0]])
 
 (def puzzle2
-  [[5 3 4 6 7 8 9 1 2]
-   [6 7 2 1 9 5 3 4 8]
-   [1 9 8 3 4 2 5 6 7]
+  [[0 0 0 6 7 0 9 0 0]
+   [0 7 0 1 9 0 3 4 8]
+   [1 9 8 3 4 2 5 0 7]
 
-   [8 5 9 7 6 1 4 2 3]
-   [4 2 6 8 5 3 7 9 1]
+   [8 5 0 7 0 1 0 2 3]
+   [4 0 6 0 5 3 7 9 1]
    [7 1 3 9 2 4 8 5 6]
 
-   [9 6 1 5 3 7 2 8 4]
-   [2 8 7 4 1 9 6 3 5]
-   [3 4 5 2 8 6 1 7 0]])
+   [0 6 1 5 3 7 2 8 4]
+   [2 8 7 4 1 9 0 3 5]
+   [3 0 0 2 8 6 1 7 0]])
 
 ; There are no duplicate digits (other than 0)
 (defn is-valid-set [s]
@@ -77,9 +77,11 @@
       (filter #(is-valid % [x y]) solutions))
     [p]))
 
-(defn solve [p]
-  (reduce (fn [puzzles cell]
-            (println (str "Looking at " (count puzzles) " solutions at " cell))
-            (mapcat #(cell-solutions % cell) puzzles))
-          [p]
-          (for [x (range 9) y (range 9)] [x y])))
+(defn solve
+  ([p]
+    (solve p (lazy-report (for [x (range 9) y (range 9)] [x y]) println 1)))
+  ([p [c & coords]]
+    (if c
+      (let [solutions (cell-solutions p c)]
+        (first (keep #(solve % coords) solutions)))
+      p))) ;If there are no more coords to check then just return what we've got
